b985b1c625b3cdecad55f8fecc9418010f690e52
Adilson Luiz de Sousa Junior <154681459+Adilson141192@users.noreply.github.com>
2024-08-13 13:01:22 -0300

    081324 aula0801

---
+++ strvt.js
@@ -0,0 +1,21 @@
+var nome = "Marcelo da Silva";
+var x = nome.length;
+console.log(x);
+
+resultado = x > 15 ? "Obrigado!" : "Por favor, preencha seu nome completo.";
+
+console.log(resultado);
+
+var res = nome.toLocaleUpperCase();
+console.log(res);
+
+
+var jogo = "Barcelona vs Real melhor time";
+var posicao = jogo.indexOf("Real");
+console.log(posicao);
+
+var cortado = jogo.slice(0,9);
+console.log(cortado);
+
+var cortado = jogo.slice(13,);
+console.log(cortado);
